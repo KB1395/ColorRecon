20095309885b943e1b87b64490587374083410ed
Paolo <paolodekeyzer@gmail.com>
2018-06-15 18:06:47 +0200

    Commented + added finalColor variable for color recuperation

--- app/src/main/java/com/unipd/paolo/colorrecon/MainActivity.java
+++ app/src/main/java/com/unipd/paolo/colorrecon/MainActivity.java
@@ -31,6 +31,7 @@ public class MainActivity extends AppCompatActivity implements CameraBridgeViewB
     private Mat hierarchy;
     private int maxcntID;
     private MatOfPoint maxcnt;
+    public int finalColor;
 
     private BaseLoaderCallback mLoaderCallback = new BaseLoaderCallback(this) {
         @Override
@@ -96,6 +97,7 @@ public class MainActivity extends AppCompatActivity implements CameraBridgeViewB
         edges = new Mat();
         hierarchy = new Mat();
         maxcnt=new MatOfPoint();
+        finalColor=0;
 
     }
 
@@ -109,31 +111,37 @@ public class MainActivity extends AppCompatActivity implements CameraBridgeViewB
 
 
 
-
+        //Trasformazione della camera frame in ogetto Mat
         mRgba = inputFrame.rgba();
         Mat mHSV = new Mat();
+        //Conversione della frame in HSV
         Imgproc.cvtColor(mRgba, mHSV, Imgproc.COLOR_RGB2HSV_FULL);
         Mat destination = new Mat(mRgba.rows(), mRgba.cols(), mRgba.type());
-
+        //ALtra conversione in grayscale (meglio per il riconoscimento di contorni)
         Imgproc.cvtColor(mRgba, destination, Imgproc.COLOR_RGB2GRAY);
         int threshold = 100;
-
+        //Effetuare un Canny sulla frame in grayscale
+        //(Canny è una trasformazione che permette di detettare i contorni)
         Imgproc.Canny(destination, edges, threshold, threshold*3);
         List<MatOfPoint> contours = new ArrayList<>();
+        //Creazione di un array di contorni
         Imgproc.findContours(edges, contours, hierarchy, Imgproc.RETR_EXTERNAL, Imgproc.CHAIN_APPROX_SIMPLE);
         MatOfPoint2f matOfPoint2f = new MatOfPoint2f();
         MatOfPoint2f approxCurve = new MatOfPoint2f();
+        //assicurarsi che almeno un contorno sia trovato
         if (!contours.isEmpty()) {
-
+            //percorrere tutti i contorni
             for (int idx = 0; idx<contours.size(); idx ++) {
 
                 MatOfPoint contour = contours.get(idx);
+                //trovare il rettangolo piu piccolo che circonda il contorno
                 Rect rect = Imgproc.boundingRect(contour);
                 double contourArea = Imgproc.contourArea(contour);
 
 
 
                 matOfPoint2f.fromList(contour.toList());
+                //Calcolo del numero di angoli e del loro valore
                 Imgproc.approxPolyDP(matOfPoint2f, approxCurve, Imgproc.arcLength(matOfPoint2f, true) * 0.02, true);
                 long total = approxCurve.total();
                 if (total >= 4 && total <= 6) {
@@ -145,41 +153,50 @@ public class MainActivity extends AppCompatActivity implements CameraBridgeViewB
                     Collections.sort(cos);
                     Double minCos = cos.get(0);
                     Double maxCos = cos.get(cos.size() - 1);
+                    //considereare solo le forme con quattro angoli e con certi valori di coseni
                     boolean isRect = total == 4 && minCos >= -0.1 && maxCos <= 0.3;
                     if (isRect) {
-                        double ratio = Math.abs(1 - (double) rect.width / rect.height);
-                        //Imgproc.drawContours(mRgba, contours, idx, color, 5);
-
+                        //Si entra in questa zona solo se il contorno è un rettangolo
                         MatOfPoint2f areaPoints = new MatOfPoint2f(contour.toArray());
+                        //Creazione di un bounding rectangle (Rettangolo che segue le rotazioni del contorno)
                         RotatedRect boundingRect = Imgproc.minAreaRect(areaPoints);
 
                         Point rotated_rect_points[] = new Point[4];
                         boundingRect.points(rotated_rect_points);
-
+                        //recuperazione del centro del rettangolo (dunque del contorno)
                         double centx = ((rect.tl().x+rect.br().x)/2);
                         double centy = (rect.tl().y+rect.br().y)/2;
 
+                        //recupero delle informazioni HSV del pixel centrale
                         double[] pixel = mHSV.get((int) centy,(int) centx);
-                        System.out.print(pixel[0]);
 
                         if(contourArea>100){
-
-
+                            //ci interessano solo i rettangoli di una certa grandezza (evitare parassiti)
                             maxcnt=contour;
                             maxcntID =idx;
                             Scalar color = new Scalar(0, 0, 255);
 
 
+                            //in funzione dei valori H S e V del pixel centrale, possiamo definire
+                            //di che colore è il rettangolo
 
+                            //il parametro recuperato sarà "finalColor" dove
+                            //0: nessun colore dettettato (default)
+                            //1: Rosso
+                            //2: Verde
                             if(pixel[0]<50 && pixel[1] >120 && pixel[2]>20 && pixel[2]<210){
-                                Imgproc.putText (mRgba,"RED",new Point(10, 50),Core.FONT_HERSHEY_SIMPLEX ,1,new Scalar(255, 255, 255),4);
+
+                                //Imgproc.putText (mRgba,"RED",new Point(10, 50),Core.FONT_HERSHEY_SIMPLEX ,1,new Scalar(255, 255, 255),4);
                                 Imgproc.drawContours(mRgba, contours, maxcntID, color, 5);
+                                finalColor=1;
+
                             }
 
 
                             else if(pixel[0]>37 && pixel[0]<100 && pixel[1] > 110 && pixel[2]>120){
-                                Imgproc.putText (mRgba,"GREEN",new Point(10, 50),Core.FONT_HERSHEY_SIMPLEX ,1,new Scalar(255, 255, 255),4);
+                                //Imgproc.putText (mRgba,"GREEN",new Point(10, 50),Core.FONT_HERSHEY_SIMPLEX ,1,new Scalar(255, 255, 255),4);
                                 Imgproc.drawContours(mRgba, contours, maxcntID, color, 5);
+                                finalColor=2;
                             }
 
                         }
@@ -194,13 +211,6 @@ public class MainActivity extends AppCompatActivity implements CameraBridgeViewB
 
 
         }
-
-        // native processing
-
-
-
-
-        //processing(mRgba.getNativeObjAddr(), mGray.getNativeObjAddr());
 
         return mRgba;
 
